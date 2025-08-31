f39da1e647fdbbdc4805839f687f9e7879879406
Juldas RAMDE <ramde266@gmail.com>
2025-08-31 07:56:53 +0000

    feat(auth)!: set up user authentication form and service

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMessage: Array<string> = [];
+
+  constructor(private router: Router, private authService: AuthenticationService) {
+  }
+
+  onLogin() {
+    this.errorMessage = [];
+    this.authService.authenticate({body: this.authRequest}).subscribe({
+      next: (response) => {
+        // todo - save the token to the localStorage
+        this.router.navigate(['/books']);
+      },
+      error: (err) => {
+        console.log(err)
+
+        // businessValidationErrors and businessErrorMessage are same name of validationErrors declared in the backend
+        if (err.error.businessValidationErrors) {
+          this.errorMessage = err.error.businessValidationErrors;
+        } else {
+          // If not a validation error, then catch the error message
+          this.errorMessage.push(err.error.businessErrorMessage);
+        }
+      }
+    })
+  }
+
+  onRegister() {
+    this.router.navigate(['/register']);
+  }
+}
